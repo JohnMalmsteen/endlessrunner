47b6111884cedf6968363ee12304f5c79f6a27ae
AndyDev2013 <sweeneyandrew2013@gmail.com>
2015-03-06 15:19:20 +0000

    Assets inc Yep

---
+++ Assets/scripts/SpawnPlatforms.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlatforms : MonoBehaviour 
+{
+	void Update () 
+	{
+		StartCoroutine("Spawn()");
+	}
+		
+	private IEnumerator Spawn()
+	{
+		
+		
+	}
+}
