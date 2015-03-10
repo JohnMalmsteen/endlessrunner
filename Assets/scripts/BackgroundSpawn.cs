7f17407868c87abfbacc2a656cb254dff48dda50
JohnMalmsteen <g00305339@gmit.ie>
2015-03-10 09:00:14 +0000

    Various Changes and Fixes Changed how the background spawns to use the same algorithm as the platforms Fixed the cloud spawning as it was accidentally calling the method twice every time a cloud got destroyed leading to cloud spam at around the 10k points mark Changed a few variables here and there

--- Assets/scripts/BackgroundSpawn.cs
+++ Assets/scripts/BackgroundSpawn.cs
@@ -3,74 +3,60 @@ using System.Collections;
 
 public class BackgroundSpawn : MonoBehaviour
 {
+	public GameObject lastSpawnedBackGround;
+	public GameObject lastSpawnedForeGround;
 	public Transform backgroundLeft;
 	public Transform foregroundLeft;
-	public Transform fogLeft;
 	public Transform backgroundRight;
 	public Transform foregroundRight;
-	public Transform fogRight;
 	public Transform cloud1;
 	public Transform cloud2;
 	public Transform cloud3;
 
-	public Vector3 spwn_back;
-	public Vector3 spwn_fore;
-	public Vector3 spwn_fog;
 	public Vector3 spwn_cloud_high;
 	public Vector3 spwn_cloud_low;
 	public Vector3 spwn_cloud_mid;
 	
 	private bool foreLeft = true;
 	private bool backLeft = true;
-	private bool fgLeft = true;
+
+
+	void Start(){
+		lastSpawnedBackGround = GameObject.Find ("backLeftspec");
+		lastSpawnedForeGround = GameObject.Find ("foreLeftspec");
+		spawnBackground ();
+		spawnForeground ();
+	}
 
 	public void spawnBackground()
 	{
-		if(backLeft)
-		{
-			Instantiate(backgroundRight, spwn_back, Quaternion.identity);
+		Transform backgroundTransform;
 
-			backLeft = false;
-		}
+		if (backLeft)
+			backgroundTransform = backgroundRight;
 		else
-		{
-			Instantiate(backgroundLeft, spwn_back, Quaternion.identity);
+			backgroundTransform = backgroundLeft;
 
-			backLeft = true;
-		}
+		lastSpawnedBackGround = ((Transform)Instantiate(backgroundTransform, new Vector3(lastSpawnedBackGround.transform.position.x + 32.63f, lastSpawnedBackGround.transform.position.y, 0f), Quaternion.identity)).gameObject;
+
+		backLeft = !backLeft;
 	}
 
 	public void spawnForeground()
 	{
-		if (foreLeft) 
-		{			
-			Instantiate (foregroundRight, spwn_fore, Quaternion.identity);
-
-			foreLeft = false;
-		}
+		Transform foregroundTransform;
+		
+		if (foreLeft)
+			foregroundTransform = foregroundRight;
 		else
-		{				
-			Instantiate(foregroundLeft, spwn_fore, Quaternion.identity); 
-
-			foreLeft = true;
-		}
+			foregroundTransform = foregroundLeft;
+		
+		lastSpawnedForeGround = ((Transform)Instantiate(foregroundTransform, new Vector3(lastSpawnedForeGround.transform.position.x + 30.63f, lastSpawnedForeGround.transform.position.y, 0f), Quaternion.identity)).gameObject;
+		
+		foreLeft = !foreLeft;
 	}
 
-	public void spawnFog()
-	{
-		if (fgLeft) 
-		{			
-			Instantiate (fogRight, spwn_fog, Quaternion.identity);
-			
-			fgLeft = false;
-		}
-		else
-		{				
-			Instantiate(fogLeft, spwn_fog, Quaternion.identity); 
-			
-			fgLeft = true;
-		}
-	}
+
 
 	public void spawnCloud()
 	{
--- Assets/scripts/KillBackground.cs
+++ Assets/scripts/KillBackground.cs
@@ -11,21 +11,15 @@ public class KillBackground : MonoBehaviour
 		
 		BackgroundSpawn spwn = backSpawner.GetComponent<BackgroundSpawn>();
 
-		if (obName == "mountainLeft" || obName == "backLeft(Clone)" || obName == "backRight(Clone)" || obName == "backLeft" || obName == "backRight") 
+		if (obName == "backLeft(Clone)" || obName == "backRight(Clone)" || obName == "backLeft" || obName == "backRight" || obName == "backLeftspec") 
 		{
 			spwn.spawnBackground();
 			Destroy(ob.gameObject);
 		}
 
-		if (obName == "nearMountains" || obName == "foreLeft(Clone)" || obName == "foreRight(Clone)" || obName == "foreLeft" || obName == "foreRight") 
+		if (obName == "foreLeft(Clone)" || obName == "foreRight(Clone)" || obName == "foreLeft" || obName == "foreRight" || obName == "foreLeftspec") 
 		{
 			spwn.spawnForeground();
-			Destroy(ob.gameObject);
-		}
-
-		if (obName == "Cloud1" || obName == "CLoud2" || obName == "Cloud3" || obName == "Cloud1(Clone)" || obName == "CLoud2(Clone)" || obName == "Cloud3(Clone)") 
-		{
-			spwn.spawnCloud();
 			Destroy(ob.gameObject);
 		}
 
